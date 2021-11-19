3366788d6584997a55423f2f6392fdfc6eff93ef
caoyj <1026637705@qq.com>
2021-11-19 11:44:27 +0800

    初始化模板

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,33 +1,18 @@
 <template>
 	<view class="index">
 		<view class="title">{{ title }}</view>
-		<button @tap="btns">点击</button>
 	</view>
 </template>
 
 <script lang="ts">
-import http from '@/http'
 import { defineComponent, ref } from 'vue'
 
 export default defineComponent({
 	setup() {
-		const title = ref('好好和')
-		const btns = () => {
-			http
-				.get<RealtimeWeather.RootObject>(
-					'https://api.caiyunapp.com/v2.5/fkBtX7WAsqBAgf6M/121.6544,25.1552/realtime.json'
-				)
-				.then((res) => {
-					console.log('res', res.timezone)
-				})
-				.catch((error) => {
-					console.log('error', error)
-				})
-		}
+		const title = ref('hello world')
 
 		return {
 			title,
-			btns,
 		}
 	},
 })
